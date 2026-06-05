9e7886fd4bff9ea6f8bc1c43757df48565cdbec8
jennifer-manriquez <jennymanb@gmail.com>
2026-06-05 16:54:15 -0600

    Chapter7-Auth-SignIn/Out

---
+++ discuss/src/app/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
+  return (
+    <div>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Not signed in</div>
+      )}
+    </div>
+  );
+}
